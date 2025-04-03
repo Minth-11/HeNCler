e827ec02b19c96095162044b549c29f63062cfdb
Zander Op de Beeck <zander.opdebeeck@gmail.com>
2025-04-03 17:26:42 +0200

    SVD->PCA

--- PCA/PCA.py
+++ PCA/PCA.py
@@ -1,6 +1,7 @@
 import os
 import scipy
 import zipfile
+import scipy as sp
 
 # inlezen data van BCOT
 
@@ -14,3 +15,19 @@ wikiMat   = scipy.io.loadmat(  wikiPath)
 acmMat    = scipy.io.loadmat(   acmPath)
 pubmedMat = scipy.io.loadmat(pubmedPath)
 dblpMat   = scipy.io.loadmat(  dblpPath)
+
+print(wikiMat["fea"]) # features # shape (2405, 4973)
+#print(wikiMat["gnd"]) # labels
+#print(wikiMat["W"]) # ???
+
+data = wikiMat
+
+features= data["fea"].astype(float)
+if not sp.issparse(features):
+      features = sp.csr_matrix(features)
+labels = data['gnd'].reshape(-1) - 1
+n_classes = len(np.unique(labels))
+
+print(features)
+
+#wikiIn =
